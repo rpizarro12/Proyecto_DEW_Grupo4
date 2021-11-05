0df8e95602840e06b94bbf5085dd620bb130ef29
Renzo Pizarro <rpizarropgo@gmail.com>
2021-11-05 00:51:09 -0500

    v1.2.2
    Formulario de registro añadido

--- FrontEnd/AmericaOrganica/src/app/access/registro/registro.component.ts
+++ FrontEnd/AmericaOrganica/src/app/access/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  registerForm = this.reg.group({
+    person: this.reg.group({
+      nombres: ['',Validators.required],
+      apellidos: ['',Validators.required],
+      dni: ['',Validators.required]
+    }),
+    email: ['', [Validators.required, Validators.email]],
+    password: ['',Validators.required],
+    acepta1: ['',Validators.required], //acepta uso de datos personales
+    acepta2: [''], //acepta suscripcion al boletin
+    acepta3: ['',Validators.required] //acepta terminos y condiciones y politicas de privacidad
+  })
 
+  constructor(private reg: FormBuilder) { }
+
+  __onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      alert("Debe completar los campos obligatorios (Tienen un *).")
+    }
+  }
   ngOnInit(): void {
   }
 
--- FrontEnd/AmericaOrganica/src/app/app.module.ts
+++ FrontEnd/AmericaOrganica/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { RegistroComponent } from './access/registro/registro.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
