2872d759abdff76770a3e398211a1de58a46b2c0
Renzo Pizarro <rpizarropgo@gmail.com>
2021-11-07 23:05:12 -0500

    v1.2.5
    Adición de formulario contactanos y pagina donde estamos

--- FrontEnd/AmericaOrganica/src/app/pages/contactanos/contactanos.component.ts
+++ FrontEnd/AmericaOrganica/src/app/pages/contactanos/contactanos.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactanos',
@@ -8,26 +7,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactanosComponent implements OnInit {
 
+  constructor() { }
 
-  contactForm = this.form.group({
-    person: this.form.group({
-      nombres: ['',Validators.required],
-      apellidos: ['',Validators.required]
-    }),
-    email: ['', [Validators.required, Validators.email]],
-    asunto: ['',Validators.required],
-    mensaje: ['',Validators.required]
-  })
-
-  constructor(private form: FormBuilder) { }
-
-  __onSubmit() {
-    if (this.contactForm.valid) {
-      console.log(this.contactForm.value);
-    } else {
-      alert("Error en formulario, hay campos vacíos.")
-    }
-  }
   ngOnInit(): void {
   }
 
